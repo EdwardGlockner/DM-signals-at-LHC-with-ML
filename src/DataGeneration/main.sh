2744042b44df3253d455cdc41cef4d321372eeaf
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-04-13 14:39:03 +0200

    bash files for HPC

---
+++ src/DataGeneration/main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# For loop
+#    rewrite spheno input params. Randomly vary mass between 150 - 500/750/1000
+#    run spheno and copy the output and use it as param_card
+#    run MG5 from the command prompt (./generate_events.py (-s -f))
+#    Copy the output generated from MA5, and delete all the other files to save storage
+#    Run MA5 and store the plots + additional data (such as mass)
+#
+#
