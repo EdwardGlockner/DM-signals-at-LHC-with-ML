b0182284044df257dd142768c07cccbf1ba5f99e
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-04-20 09:32:00 +0200

    pulling

--- src/ML_models/Edward/CNN/classification/CNN.py
+++ src/ML_models/Edward/CNN/classification/CNN.py
@@ -6,6 +6,18 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 class CNN_model():
     def __init__(self, X_train, y_train, X_test, y_test, epochs=5):
+        """
+        asdfasdf
+
+        @arguments:
+            X_train: <numpy.ndarray>
+            y_train: <tensorflow.python.framework.ops.EagerTensor>
+            X_test:  <numpy.ndarray>
+            y_test:  <tensorflow.python.framework.ops.EagerTensor>
+            epochs:  <int>
+
+        """
+        print(type(y_train))
         self.X_train = X_train
         self.y_train = y_train
         self.X_test = X_test
@@ -16,7 +28,18 @@ class CNN_model():
         self.callback = Callback()
 
 
-    def _create_model(self):
+    def _create_modeltensorflow.python.framework.ops.EagerTensortensorflow.python.framework.ops.EagerTensor(self):
+        """
+        asdfasdf
+
+        @arguments:
+            None
+
+        @returns:
+            None
+
+        """
+        # Add all the layers
         model = models.Sequential()
         model.add(layers.Conv2D(32, (3, 3), activation= "relu", input_shape = (28, 28, 1)))
         model.add(layers.BatchNormalization())
@@ -24,21 +47,27 @@ class CNN_model():
         model.add(layers.Conv2D(32, (3, 3), activation = "relu"))
         model.add(layers.BatchNormalization())
         model.add(layers.MaxPooling2D((2, 2)))
-        #model.add(layers.Conv2D(32, (3, 3), activation = "relu"))
-        #model.add(layers.MaxPooling2D((2, 2)))
-        #model.add(layers.BatchNormalization())
-        # three splits in data
         model.add(layers.Flatten())
         model.add(layers.Dense(64, activation = "relu"))
         model.add(layers.BatchNormalization())
         model.add(layers.Dense(16, activation = "relu"))
         model.add(layers.Dense(10, activation = "softmax"))
-
+        
+        print(model.summary())
         return model
 
 
     def compile(self, model_name):
-        print(self.model.summary())
+        """
+        asdfasdf
+
+        @arguments:
+            model_name: <string> Name of the figure that will be saved
+
+        @returns:
+            None
+        """
+        # Save figure of the model
         tf.keras.utils.plot_model(
             self.model,
             to_file= model_name + ".png",
@@ -48,13 +77,23 @@ class CNN_model():
             expand_nested=True,
             dpi=96,
         )
-
+        
+        # Compile the model
         self.model.compile(optimizer = "adam",
                       loss = tf.keras.losses.CategoricalCrossentropy(from_logits=False),
                       metrics = ["accuracy"])
 
     
     def train(self, print_perf=True):
+        """
+        asdfasdf
+
+        @arguments:
+            print_perf: <bool> 
+
+        @returns:
+            None
+        """
         self.history = self.model.fit(self.X_train, self.y_train, epochs = 1000,
                             validation_data = (self.X_test, self.y_test), callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', 
                                 min_delta=0, 
@@ -70,6 +109,10 @@ class CNN_model():
 
 
     def plot_performance(self):
+        """
+        asdfasdfasdf
+
+        """
         plt.plot(self.history.history["accuracy"], label = "accuracy")
         plt.plot(self.history.history["val_accuracy"], label = "val_accuracy")
         plt.xlabel("Epochs")
