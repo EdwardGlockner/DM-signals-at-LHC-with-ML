fea9056dd2f85fce2aa0c798554325839debc6b4
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-04-24 14:17:17 +0200

    preparing for UPPMAX

--- src/ML/main_training.py
+++ src/ML/main_training.py
@@ -6,6 +6,8 @@ import os
 sys.path.append(str(sys.path[0][:-14]))
 dirname = os.getcwd()
 dirname = dirname.replace("src/ML","")
+sys.path.insert(1, os.path.join(dirname, "src/ML/data_preparation"))
+sys.path.insert(1, os.path.join(dirname, "src/ML/models"))
 
 #---LOCAL IMPORTS--------+
 from classification_bCNN import *
@@ -62,7 +64,6 @@ def get_sets(folder_img_path, folder_target_path, img_height, img_width):
     
     if len(img_arr) != len(target_vals):
         print("Error in function <get_sets>. Arrays img_arr and target_vals are not the same size.")
-        return None
 
     if len(img_arr) != len(class_labels):
         print("Erorr in function <get_sets>. Arrays img_arr and class_labels are not the same size.")
@@ -77,7 +78,7 @@ def get_sets(folder_img_path, folder_target_path, img_height, img_width):
             [X_train_re, y_train_re, X_test_re, y_test_re, X_val_re, y_val_re]
 
 
-def train_classification():
+def train_classification(X_train, y_train, X_test, y_test):
     """
     asdfasdf
 
@@ -89,7 +90,7 @@ def train_classification():
     pass
 
 
-def train_regression():
+def train_regression(X_train, y_train, X_test, y_test):
     """
     asdfasdf
 
@@ -120,7 +121,11 @@ def main():
     
     preprocess(folder_img_path) 
     cl, re =  get_sets(folder_img_path, folder_target_path, img_height, img_width)
+    X_val_cl, y_val_cl = cl[4], cl[5]
+    X_val_re, y_val_re = re[4], re[5]
 
+    train_classification(cl[0], cl[1], cl[2], cl[3])
+    train_regression(re[0], re[1], re[2], re[3])
 
 #---RUN CODE-------------+
 if __name__ == "__main__":
