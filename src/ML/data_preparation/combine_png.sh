d21b92d746b83a1af9c9b373982120efc2e533a2
aimaax <maxandersson8@outlook.com>
2023-04-24 15:39:40 +0200

    Updated combine

--- src/ML/data_preparation/combine_png.sh
+++ src/ML/data_preparation/combine_png.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
+# This script is only for a directory with the same model
+
 current_dir=$(pwd)
 
-input_directory="${current_dir}/Storage_data/Test_collapse"
-output_directory="${current_dir}/Storage_data/MSSMmonojet"
+input_directory="$1"
+output_directory="$2"
 
-
-#convert +append ${input_directory}/*.png ${output_directory}/combined.png
-
-
-# Extract the highest index in directory
-
-highest_index=$(ls ${current_dir}/Storage_data/MSSMmonojet/MET/*_neutralino_MET.png | \
-		awk '{gsub(".*/|_neutralino_MET.png","");print}' | sort -n  | tail -1 )
-
-echo "Highest index: $highest_index"
-
-for (( i=0; i<highest_index; i++ ))
-do 	
-	convert +append ${output_directory}/ALL_png/${i}*.png ${output_directory}/Combined/combined_${i}.png
+# Combine the average pngs in a model directory
+for (( i=0; i<10; i++ ))
+do 
+		convert ${input_directory}/${i}*_ETA.png \
+			${input_directory}/${i}*_MET.png \
+			${input_directory}/${i}*_PT.png +append ${output_directory}/${i}_combined.png
 done
 
-# Convert all of the images indices [0 to 17] from the folders and combine them together
-
-
-
-
-echo "Combined PNG image created at ${output_directory}/combined.png"
