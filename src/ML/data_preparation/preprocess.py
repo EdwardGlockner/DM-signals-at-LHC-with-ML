7940c6ccf7da3b8e4f7e51791bf9ba48174b4acf
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-04-25 13:42:21 +0200

    data_prep file structure

--- src/ML/data_preparation/preprocess.py
+++ src/ML/data_preparation/preprocess.py
@@ -5,6 +5,58 @@ import cv2
 from PIL import Image
 
 #---FUNCTIONS--------+
+
+def average_imgs(folder_path, folder_dest, show=False):
+    """
+    Takes in a path to a folder, and averages the pixelvalues of the images
+    in the folder. The output image is saved and returned.
+
+    This functions requires all images to be the same size
+
+    @params:
+        folder_path: <string> Full path to the folder where the images are
+        show: <bool> Visualize image or note
+        folder_dest <string> Path to the folder where the files should be saved
+    @returns:
+        None
+    """
+    # Read all files
+    files = os.listdir(folder_path)
+    imgs = [folder_path + file for file in files if file[-4:] == ".png"]
+
+    # Group all the images based on first index, and image type (last 7 characters)
+    img_grouping = {}
+    for i in range(len(imgs)):
+        temp_img = imgs[i].split("/")[-1]
+        temp_key = (temp_img[0], temp_img[-7:])
+
+        if temp_key in img_grouping:
+            img_grouping[temp_key].append(imgs[i])
+        else:
+            img_grouping[temp_key] = [imgs[i]]
+
+    # Convert into list
+    img_grouping = [v for k, v in img_grouping.items()]
+    
+    # Averaging all the images, and saving them
+    for i in range(len(img_grouping)):
+        imgs = [np.array(Image.open(img)) for img in img_grouping[i]]
+        arrs = [np.array(img) for img in imgs]
+        avg_arr = np.mean(arrs, axis=0).astype(float)
+        
+        avg_img = Image.fromarray(avg_arr)
+        avg_img = avg_img.convert("L") # Convert to grayscale
+        # For RGD: ...convert("RGB")
+        
+        new_file_name = img_grouping[i][0].split("/")[-1]
+        new_file_name = new_file_name[0:2] + new_file_name[4:]
+
+        avg_img.save(folder_dest + new_file_name)
+        
+        if show:
+            avg_img.show()
+
+
 def clear_img_directory(folder_path):
     """
     Removes all images in a directory
